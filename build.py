29f0415a1b8ef2bb2f70938b3676039fdbe9aa03
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-09-13 17:20:31 -0500

    Adds slides for State of Playdoh at Mozilla All Hands Q3, 2011

--- build.py
+++ build.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+import os
 import optparse
 from subprocess import check_call
+import codecs
 
 def main():
-    p = optparse.OptionParser(usage='%prog [options]')
+    p = optparse.OptionParser(usage='%prog [options] slides-source.txt')
     (options, args) = p.parse_args()
-    check_call(['./custom_rst2s5.py', './talk.txt', './index.html'])
-    # check_call(['open', './index.html'])
+    if len(args) != 1:
+        p.error('Incorrect args')
+    source = args[0]
+    output = '%s.html' % os.path.splitext(source)[0]
+    check_call(['./custom_rst2s5.py', source, output])
+    print "Wrote %s" % output
 
 if __name__ == '__main__':
     main()
